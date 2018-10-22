3e98281921c283d5934e6b8de627288f693a2abf
Urjit Singh Bhatia <urjitsb87@gmail.com>
2018-10-22 01:11:52 +0000

    Aggresive GC :)

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"runtime/debug"
 
 	"github.com/urjitbhatia/goyaad/cmd"
 )
 
 func main() {
 	// logrus.SetLevel(logrus.DebugLevel)
+	// More Aggresive GC
+	debug.SetGCPercent(5)
 	go func() {
 		log.Println(http.ListenAndServe(":6060", nil))
 	}()
