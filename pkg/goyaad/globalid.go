16b1a0a74acd7af0b02651dd3240d1a16f7e4a64
Urjit Singh Bhatia <urjitsb87@gmail.com>
2018-12-17 01:48:49 +0000

    Remove unused idMutext from globalId

--- pkg/goyaad/globalid.go
+++ pkg/goyaad/globalid.go
@@ -1,12 +1,10 @@
 package goyaad
 
 import (
-	"sync"
 	"sync/atomic"
 )
 
 var globalIDCtr uint64
-var idMutex = sync.Mutex{}
 
 // NextID generates the next int id monotonically increasing
 func NextID() uint64 {
