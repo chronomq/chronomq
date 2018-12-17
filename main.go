urjitbhatia/yaad

Add pprof debug endpoint

main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/urjitbhatia/yaad/cmd"
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/urjitbhatia/yaad/cmd"
+)
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe(":6060", nil))
+	}()
 	cmd.Execute()
 }

chronomq/chronomq

main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/urjitbhatia/goyaad/cmd"
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/urjitbhatia/goyaad/cmd"
+)
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe(":6060", nil))
+	}()
 	cmd.Execute()
 }