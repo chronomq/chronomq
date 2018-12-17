urjitbhatia/yaad

Implement simpler, current spoke based design

pkg/yaad/temporal.go
@@ -1,0 +1,13 @@
+package yaad
+
+// TemporalState is a classification of time for an object
+type TemporalState int
+
+const (
+	// Past in time
+	Past TemporalState = iota
+	// Current in time
+	Current
+	// Future in time
+	Future
+)

chronomq/chronomq

pkg/goyaad/temporal.go
@@ -1,0 +1,13 @@
+package goyaad
+
+// TemporalState is a classification of time for an object
+type TemporalState int
+
+const (
+	// Past in time
+	Past TemporalState = iota
+	// Current in time
+	Current
+	// Future in time
+	Future
+)