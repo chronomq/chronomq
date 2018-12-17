urjitbhatia/yaad

change to cobra cli

cmd/root.go
@@ -1,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/urjitbhatia/yaad/pkg/protocol"
+)
+
+var rootCmd = &cobra.Command{
+	Short: "yaad",
+	Run: func(cmd *cobra.Command, args []string) {
+		runServer()
+	},
+}
+
+func runServer() {
+	logrus.Info("I AM GROOT!")
+	// logrus.SetLevel(logrus.DebugLevel)
+
+	s := protocol.NewYaadServer()
+
+	log.Fatal(s.ListenAndServe("tcp", ":11300"))
+}
+
+// Execute root cmd by default
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}

main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"log"
-
-	"github.com/sirupsen/logrus"
-	"github.com/urjitbhatia/yaad/pkg/protocol"
-)
+import "github.com/urjitbhatia/yaad/cmd"
 
 func main() {
-	logrus.Info("I AM GROOT!")
-	// logrus.SetLevel(logrus.DebugLevel)
-
-	s := protocol.NewYaadServer()
-
-	log.Fatal(s.ListenAndServe("tcp", ":11300"))
+	cmd.Execute()
 }

chronomq/chronomq

cmd/root.go
@@ -1,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/urjitbhatia/goyaad/pkg/protocol"
+)
+
+var rootCmd = &cobra.Command{
+	Short: "goyaad",
+	Run: func(cmd *cobra.Command, args []string) {
+		runServer()
+	},
+}
+
+func runServer() {
+	logrus.Info("I AM GROOT!")
+	// logrus.SetLevel(logrus.DebugLevel)
+
+	s := protocol.NewYaadServer()
+
+	log.Fatal(s.ListenAndServe("tcp", ":11300"))
+}
+
+// Execute root cmd by default
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}

main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"log"
-
-	"github.com/sirupsen/logrus"
-	"github.com/urjitbhatia/goyaad/pkg/protocol"
-)
+import "github.com/urjitbhatia/goyaad/cmd"
 
 func main() {
-	logrus.Info("I AM GROOT!")
-	// logrus.SetLevel(logrus.DebugLevel)
-
-	s := protocol.NewYaadServer()
-
-	log.Fatal(s.ListenAndServe("tcp", ":11300"))
+	cmd.Execute()
 }