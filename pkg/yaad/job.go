urjitbhatia/yaad

Add job and spoke implementation of yaad in go

pkg/yaad/job.go
@@ -1,0 +1,79 @@
+package yaad
+
+import (
+	"errors"
+	"time"
+
+	"github.com/satori/go.uuid"
+)
+
+// Job is the basic unit of work in yaad
+type Job struct {
+	id        uuid.UUID
+	triggerAt time.Time
+	body      *[]byte
+}
+
+// Impl Job
+
+//NewJob creates a new yaad job
+func NewJob(id uuid.UUID, triggerAt time.Time, b *[]byte) (*Job, error) {
+	switch id.Version() {
+	case 4:
+		return &Job{
+			id:        id,
+			triggerAt: triggerAt,
+			body:      b,
+		}, nil
+	default:
+		return nil, errors.New("Only uuid v4 ids are accepted")
+	}
+}
+
+// NewJobAutoID creates a job a job id assigned automatically
+func NewJobAutoID(triggerAt time.Time, b *[]byte) (*Job, error) {
+	return &Job{
+		id:        uuid.NewV4(),
+		triggerAt: triggerAt,
+		body:      b,
+	}, nil
+}
+
+// ID returns the id of the job
+func (j *Job) ID() uuid.UUID {
+	return j.id
+}
+
+// Body returns the job of the job
+func (j *Job) Body() *[]byte {
+	return j.body
+}
+
+// TriggerAt returns the job's trigger time
+func (j *Job) TriggerAt() time.Time {
+	return j.triggerAt
+}
+
+// IsReady returns true if job is ready to be worked on
+func (j *Job) IsReady() bool {
+	return time.Now().After(j.triggerAt)
+}
+
+// JobsByTime implements sort.Interface for a collection of jobs //
+type JobsByTime []*Job
+
+// Len is the number of elements in the collection.
+func (b JobsByTime) Len() int {
+	return len(b)
+}
+
+// Less reports whether the element with
+// index i should sort before the element with index j.
+func (b JobsByTime) Less(i, j int) bool {
+	return b[i].triggerAt.Before(b[j].triggerAt)
+}
+
+// Swap swaps the elements with indexes i and j.
+func (b JobsByTime) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}

pkg/yaad/job_test.go
@@ -1,0 +1,45 @@
+package yaad_test
+
+import (
+	"sort"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	uuid "github.com/satori/go.uuid"
+	. "github.com/urjitbhatia/yaad/pkg/yaad"
+)
+
+var _ = Describe("Test jobs", func() {
+	Context("Basic job tests", func() {
+		It("can create a job", func() {
+			b := []byte("foo")
+			_, err := NewJob(uuid.NewV4(), time.Now(), &b)
+			Expect(err).To(BeNil())
+
+			_, err = NewJob(uuid.NewV4(), time.Now(), nil)
+			Expect(err).To(BeNil())
+
+			j, err := NewJobAutoID(time.Now(), nil)
+			Expect(err).To(BeNil())
+			Expect(j.ID()).To(BeAssignableToTypeOf(uuid.NewV4()))
+		})
+	})
+
+	Context("Job Ordering", func() {
+		It("orders jobs correctly", func() {
+			t := time.Now()
+			jone, _ := NewJobAutoID(t.Add(1), nil)
+			jtwo, _ := NewJobAutoID(t.Add(20), nil)
+			jthree, _ := NewJobAutoID(t.Add(50), nil)
+
+			jobs := JobsByTime{jtwo, jone, jthree}
+			sort.Sort(jobs)
+
+			Expect(jobs[0].ID()).To(Equal(jone.ID()))
+			Expect(jobs[1].ID()).To(Equal(jtwo.ID()))
+			Expect(jobs[2].ID()).To(Equal(jthree.ID()))
+		})
+	})
+})

pkg/yaad/spoke.go
@@ -1,0 +1,167 @@
+package yaad
+
+import (
+	"sort"
+	"time"
+
+	"github.com/satori/go.uuid"
+	"github.com/sirupsen/logrus"
+)
+
+// Spoke is a time bound chain of jobs
+type Spoke struct {
+	id uuid.UUID
+	*spokeBound
+	jobMap   map[uuid.UUID]*Job
+	jobQueue JobsByTime
+}
+
+// spokeBound defines time bounds for a spoke
+type spokeBound struct {
+	start time.Time
+	end   time.Time
+}
+
+// -- SpokeBound -- //
+// Start returns the starting time of this spoke bound (inclusive)
+func (sb *spokeBound) Start() time.Time {
+	return sb.start
+}
+
+// End returns the ending time of this spoke bound (exclusive)
+func (sb *spokeBound) End() time.Time {
+	return sb.end
+}
+
+// Contains returns true if sb fully contains o
+func (sb *spokeBound) Contains(o *spokeBound) bool {
+	return sb.start.Sub(o.start) <= 0 && sb.end.After(o.end)
+}
+
+// ContainsJob returns true if this job is bounded by this spoke
+func (sb *spokeBound) ContainsJob(j *Job) bool {
+	return sb.start.Sub(j.triggerAt) <= 0 && sb.end.After(j.triggerAt)
+}
+
+// IsReady returns true if SpokeBound started in the past
+// This spoke bound may end in the future
+func (sb *spokeBound) IsReady() bool {
+	return time.Now().After(sb.start)
+}
+
+// IsExpired returns true if SpokeBound ended in the past
+func (sb *spokeBound) IsExpired() bool {
+	return time.Now().After(sb.end)
+}
+
+// -- Spoke -- //
+
+// NewSpokeFromNow creates a new spoke to hold jobs that starts from now
+// and ends at the given duration
+func NewSpokeFromNow(duration time.Duration) *Spoke {
+	now := time.Now()
+	return NewSpoke(now, now.Add(duration))
+}
+
+// NewSpoke creates a new spoke to hold jobs
+func NewSpoke(start, end time.Time) *Spoke {
+	return &Spoke{id: uuid.NewV4(),
+		jobMap:     make(map[uuid.UUID]*Job),
+		jobQueue:   JobsByTime{},
+		spokeBound: &spokeBound{start, end}}
+}
+
+// AddJob submits a job to the spoke. If the spoke cannot take responsibility
+// of this job, it will return it as it is, otherwise nil is returned
+func (s *Spoke) AddJob(j *Job) *Job {
+	if s.IsExpired() {
+		return j
+	}
+
+	if s.ContainsJob(j) {
+		logrus.WithFields(logrus.Fields{"job": j,
+			"spoke": s}).Debug("Accepting job")
+		s.jobMap[j.id] = j
+		s.jobQueue = append(s.jobQueue, j)
+		return nil
+	}
+
+	return j
+}
+
+// Walk returns a pointer to an array of pointers to Jobs
+// (no copy operations)
+func (s *Spoke) Walk() *[]*Job {
+	ready := []*Job{}
+
+	if len(s.jobQueue) == 0 {
+		return &ready
+	}
+
+	sort.Sort(s.jobQueue)
+	var j *Job
+	for {
+		if len(s.jobQueue) == 0 {
+			break
+		}
+
+		if time.Now().Before(s.jobQueue[0].triggerAt) {
+			break
+		}
+
+		j, s.jobQueue = s.jobQueue[0], s.jobQueue[1:]
+		delete(s.jobMap, j.id)
+		ready = append(ready, j)
+
+	}
+	return &ready
+}
+
+// CancelJob will try to delete a job that hasn't been consumed yet
+func (s *Spoke) CancelJob(id uuid.UUID) {
+	if _, ok := s.jobMap[id]; ok {
+		delete(s.jobMap, id)
+		for i, j := range s.jobQueue {
+			if j.id == id {
+				s.jobQueue = append(s.jobQueue[:i], s.jobQueue[i+1:]...)
+				break
+			}
+		}
+	}
+}
+
+// OwnsJob returns true if a job by given id is owned by this spoke
+func (s *Spoke) OwnsJob(id uuid.UUID) bool {
+	_, ok := s.jobMap[id]
+	return ok
+}
+
+// PendingJobsLen returns the number of jobs remaining in this spoke
+func (s *Spoke) PendingJobsLen() int {
+	return len(s.jobMap)
+}
+
+// ID returns the id of this spoke
+func (s *Spoke) ID() uuid.UUID {
+	return s.id
+}
+
+// -- Implement Sort.Interface on Spokes ---//
+
+// SpokesByTime implements sort.Interface for Spokes
+type SpokesByTime []*Spoke
+
+func (b SpokesByTime) Len() int {
+	return len(b)
+}
+
+func (b SpokesByTime) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
+
+func (b SpokesByTime) Less(i, j int) bool {
+	// i starts before or at the same time as j
+	return b[i].start.Sub(b[j].start) <= 0 &&
+		// && i ends before or at the same time as j
+		b[i].end.Sub(b[j].end) <= 0
+}

pkg/yaad/spoke_test.go
@@ -1,0 +1,171 @@
+package yaad_test
+
+import (
+	"math/rand"
+	"sort"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	uuid "github.com/satori/go.uuid"
+	. "github.com/urjitbhatia/yaad/pkg/yaad"
+)
+
+var _ = Describe("Test spokes", func() {
+	Context("Basic spoke tests", func() {
+		It("can create a spoke", func() {
+			t := time.Now()
+
+			// Starts in an hour, ends in 2 hours
+			s := NewSpoke(t.Add(time.Hour*1), t.Add(time.Hour*2))
+			Expect(s.ID()).To(BeAssignableToTypeOf(uuid.NewV4()))
+			Expect(s.IsReady()).To(BeFalse())
+			Expect(s.IsExpired()).To(BeFalse())
+
+			// Starts now, ends in 10 hours
+			s = NewSpokeFromNow(time.Hour * 10)
+			Expect(s.ID()).To(BeAssignableToTypeOf(uuid.NewV4()))
+			Expect(s.IsReady()).To(BeTrue())
+			Expect(s.IsExpired()).To(BeFalse())
+
+			// Ends in the past for the next tick
+			s = NewSpokeFromNow(0)
+			Expect(s.ID()).To(BeAssignableToTypeOf(uuid.NewV4()))
+			Expect(s.IsReady()).To(BeTrue())
+			Expect(s.IsExpired()).To(BeTrue())
+		})
+
+		It("accepts jobs into spoke", func() {
+			s := NewSpokeFromNow(time.Hour * 1)
+			j, err := NewJobAutoID(s.Start().Add(time.Second*20), nil)
+			Expect(err).To(BeNil())
+
+			// Accepts the job and returns nil
+			Expect(s.ContainsJob(j)).To(BeTrue())
+			Expect(s.AddJob(j)).To(BeNil())
+			Expect(s.PendingJobsLen()).To(Equal(1))
+			Expect(s.OwnsJob(j.ID())).To(BeTrue())
+		})
+
+		It("rejects jobs from spoke that lie outside its time bounds", func() {
+			now := time.Now()
+			s := NewSpoke(now.Add(time.Hour*1), now.Add(time.Hour*2))
+
+			// Job triggers after spoke ends
+			j, err := NewJobAutoID(s.End().Add(time.Hour*1), nil)
+			Expect(err).To(BeNil())
+
+			// Rejects the job and returns it
+			Expect(s.AddJob(j)).To(Equal(j))
+			Expect(s.PendingJobsLen()).To(Equal(0))
+
+			// Job triggers before spoke ends
+			j, err = NewJobAutoID(s.Start().Add(-10*time.Minute), nil)
+			Expect(err).To(BeNil())
+
+			// Rejects the job and returns it
+			Expect(s.AddJob(j)).To(Equal(j))
+			Expect(s.PendingJobsLen()).To(Equal(0))
+		})
+
+		It("walks empty spoke", func() {
+			s := NewSpokeFromNow(time.Minute * 1)
+			Expect(len(*s.Walk())).To(Equal(0))
+		})
+
+		It("walks spoke with jobs", func() {
+			s := NewSpokeFromNow(time.Hour * 1)
+			Expect(len(*s.Walk())).To(Equal(0))
+
+			for i := 0; i < 10; i++ {
+				j, _ := NewJobAutoID(s.Start().Add(time.Nanosecond*time.Duration(rand.Intn(900))), nil)
+				Expect(s.AddJob(j)).To(BeNil())
+			}
+			Expect(s.PendingJobsLen()).To(Equal(10))
+
+			// Wait for all jobs to be ready
+			time.Sleep(time.Second * 1)
+
+			jobs := *s.Walk()
+			Expect(len(jobs)).To(Equal(10))
+			prev := jobs[0]
+			for i := 1; i < len(jobs); i++ {
+				// Walk returns jobs in order
+				Expect(prev.TriggerAt().Sub(jobs[i].TriggerAt()) <= 0).To(BeTrue())
+			}
+		})
+
+		It("repeated walks spoke with jobs as they expire", func() {
+			s := NewSpokeFromNow(time.Hour * 1)
+			Expect(len(*s.Walk())).To(Equal(0))
+
+			// Add some jobs < 1 sec triggerAt
+			for i := 0; i < 10; i++ {
+				j, _ := NewJobAutoID(s.Start().Add(time.Nanosecond*time.Duration(rand.Intn(900))), nil)
+				Expect(s.AddJob(j)).To(BeNil())
+			}
+			Expect(s.PendingJobsLen()).To(Equal(10))
+			// Add some jobs > 1 sec triggerAt
+			for i := 0; i < 10; i++ {
+				j, _ := NewJobAutoID(s.Start().Add(time.Minute*time.Duration(10+rand.Intn(40))), nil)
+				Expect(s.AddJob(j)).To(BeNil())
+			}
+			Expect(s.PendingJobsLen()).To(Equal(20))
+
+			// Wait for all jobs to be ready
+			time.Sleep(time.Second * 1)
+
+			jobs := *s.Walk()
+			Expect(len(jobs)).To(Equal(10))
+			prev := jobs[0]
+			for i := 1; i < len(jobs); i++ {
+				// Walk returns jobs in order
+				Expect(prev.TriggerAt().Sub(jobs[i].TriggerAt()) <= 0).To(BeTrue())
+			}
+
+			// 10 jobs should remain
+			Expect(s.PendingJobsLen()).To(Equal(10))
+
+			// Walk is idempotent
+			jobs = *s.Walk()
+			Expect(len(jobs)).To(Equal(0))
+			Expect(s.PendingJobsLen()).To(Equal(10))
+		})
+
+		It("cancels job from spoke", func() {
+			s := NewSpokeFromNow(time.Hour * 1)
+			Expect(s.PendingJobsLen()).To(Equal(0))
+
+			j, _ := NewJobAutoID(s.Start().Add(time.Minute*10), nil)
+			Expect(s.AddJob(j)).To(BeNil())
+			Expect(s.PendingJobsLen()).To(Equal(1))
+
+			s.CancelJob(j.ID())
+			Expect(s.PendingJobsLen()).To(Equal(0))
+		})
+
+		It("cancels job from spoke that doesnt exist does nothing", func() {
+			s := NewSpokeFromNow(time.Hour * 1)
+			Expect(s.PendingJobsLen()).To(Equal(0))
+			s.CancelJob(uuid.NewV4())
+			s.CancelJob(uuid.NewV4())
+		})
+	})
+
+	Context("Spoke Ordering", func() {
+		It("orders spokes correctly", func() {
+			t := time.Now()
+			sone := NewSpoke(t.Add(1), t.Add(10))
+			stwo := NewSpoke(t.Add(20), t.Add(30))
+			sthree := NewSpoke(t.Add(50), t.Add(55))
+
+			spokes := SpokesByTime{stwo, sone, sthree}
+			sort.Sort(spokes)
+
+			Expect(spokes[0].ID()).To(Equal(sone.ID()))
+			Expect(spokes[1].ID()).To(Equal(stwo.ID()))
+			Expect(spokes[2].ID()).To(Equal(sthree.ID()))
+		})
+	})
+})

pkg/yaad/yaad_suite_test.go
@@ -1,0 +1,13 @@
+package yaad_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestYaad(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Yaad Suite")
+}

chronomq/chronomq

pkg/yaad/job.go
@@ -1,0 +1,79 @@
+package goyaad
+
+import (
+	"errors"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+// Job is the basic unit of work in yaad
+type Job struct {
+	id        uuid.UUID
+	triggerAt time.Time
+	body      *[]byte
+}
+
+// Impl Job
+
+//NewJob creates a new yaad job
+func NewJob(id uuid.UUID, triggerAt time.Time, b *[]byte) (*Job, error) {
+	switch id.Version() {
+	case 4:
+		return &Job{
+			id:        id,
+			triggerAt: triggerAt,
+			body:      b,
+		}, nil
+	default:
+		return nil, errors.New("Only uuid v4 ids are accepted")
+	}
+}
+
+// NewJobAutoID creates a job a job id assigned automatically
+func NewJobAutoID(triggerAt time.Time, b *[]byte) (*Job, error) {
+	return &Job{
+		id:        uuid.NewV4(),
+		triggerAt: triggerAt,
+		body:      b,
+	}, nil
+}
+
+// ID returns the id of the job
+func (j *Job) ID() uuid.UUID {
+	return j.id
+}
+
+// Body returns the job of the job
+func (j *Job) Body() *[]byte {
+	return j.body
+}
+
+// TriggerAt returns the job's trigger time
+func (j *Job) TriggerAt() time.Time {
+	return j.triggerAt
+}
+
+// IsReady returns true if job is ready to be worked on
+func (j *Job) IsReady() bool {
+	return time.Now().After(j.triggerAt)
+}
+
+// JobsByTime implements sort.Interface for a collection of jobs //
+type JobsByTime []*Job
+
+// Len is the number of elements in the collection.
+func (b JobsByTime) Len() int {
+	return len(b)
+}
+
+// Less reports whether the element with
+// index i should sort before the element with index j.
+func (b JobsByTime) Less(i, j int) bool {
+	return b[i].triggerAt.Before(b[j].triggerAt)
+}
+
+// Swap swaps the elements with indexes i and j.
+func (b JobsByTime) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}

pkg/yaad/job_test.go
@@ -1,0 +1,45 @@
+package goyaad_test
+
+import (
+	"sort"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	uuid "github.com/satori/go.uuid"
+	. "github.com/urjitbhatia/goyaad/pkg/yaad"
+)
+
+var _ = Describe("Test jobs", func() {
+	Context("Basic job tests", func() {
+		It("can create a job", func() {
+			b := []byte("foo")
+			_, err := NewJob(uuid.NewV4(), time.Now(), &b)
+			Expect(err).To(BeNil())
+
+			_, err = NewJob(uuid.NewV4(), time.Now(), nil)
+			Expect(err).To(BeNil())
+
+			j, err := NewJobAutoID(time.Now(), nil)
+			Expect(err).To(BeNil())
+			Expect(j.ID()).To(BeAssignableToTypeOf(uuid.NewV4()))
+		})
+	})
+
+	Context("Job Ordering", func() {
+		It("orders jobs correctly", func() {
+			t := time.Now()
+			jone, _ := NewJobAutoID(t.Add(1), nil)
+			jtwo, _ := NewJobAutoID(t.Add(20), nil)
+			jthree, _ := NewJobAutoID(t.Add(50), nil)
+
+			jobs := JobsByTime{jtwo, jone, jthree}
+			sort.Sort(jobs)
+
+			Expect(jobs[0].ID()).To(Equal(jone.ID()))
+			Expect(jobs[1].ID()).To(Equal(jtwo.ID()))
+			Expect(jobs[2].ID()).To(Equal(jthree.ID()))
+		})
+	})
+})

pkg/yaad/spoke.go
@@ -1,0 +1,167 @@
+package goyaad
+
+import (
+	"sort"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/sirupsen/logrus"
+)
+
+// Spoke is a time bound chain of jobs
+type Spoke struct {
+	id uuid.UUID
+	*spokeBound
+	jobMap   map[uuid.UUID]*Job
+	jobQueue JobsByTime
+}
+
+// spokeBound defines time bounds for a spoke
+type spokeBound struct {
+	start time.Time
+	end   time.Time
+}
+
+// -- SpokeBound -- //
+// Start returns the starting time of this spoke bound (inclusive)
+func (sb *spokeBound) Start() time.Time {
+	return sb.start
+}
+
+// End returns the ending time of this spoke bound (exclusive)
+func (sb *spokeBound) End() time.Time {
+	return sb.end
+}
+
+// Contains returns true if sb fully contains o
+func (sb *spokeBound) Contains(o *spokeBound) bool {
+	return sb.start.Sub(o.start) <= 0 && sb.end.After(o.end)
+}
+
+// ContainsJob returns true if this job is bounded by this spoke
+func (sb *spokeBound) ContainsJob(j *Job) bool {
+	return sb.start.Sub(j.triggerAt) <= 0 && sb.end.After(j.triggerAt)
+}
+
+// IsReady returns true if SpokeBound started in the past
+// This spoke bound may end in the future
+func (sb *spokeBound) IsReady() bool {
+	return time.Now().After(sb.start)
+}
+
+// IsExpired returns true if SpokeBound ended in the past
+func (sb *spokeBound) IsExpired() bool {
+	return time.Now().After(sb.end)
+}
+
+// -- Spoke -- //
+
+// NewSpokeFromNow creates a new spoke to hold jobs that starts from now
+// and ends at the given duration
+func NewSpokeFromNow(duration time.Duration) *Spoke {
+	now := time.Now()
+	return NewSpoke(now, now.Add(duration))
+}
+
+// NewSpoke creates a new spoke to hold jobs
+func NewSpoke(start, end time.Time) *Spoke {
+	return &Spoke{id: uuid.NewV4(),
+		jobMap:     make(map[uuid.UUID]*Job),
+		jobQueue:   JobsByTime{},
+		spokeBound: &spokeBound{start, end}}
+}
+
+// AddJob submits a job to the spoke. If the spoke cannot take responsibility
+// of this job, it will return it as it is, otherwise nil is returned
+func (s *Spoke) AddJob(j *Job) *Job {
+	if s.IsExpired() {
+		return j
+	}
+
+	if s.ContainsJob(j) {
+		logrus.WithFields(logrus.Fields{"job": j,
+			"spoke": s}).Debug("Accepting job")
+		s.jobMap[j.id] = j
+		s.jobQueue = append(s.jobQueue, j)
+		return nil
+	}
+
+	return j
+}
+
+// Walk returns a pointer to an array of pointers to Jobs
+// (no copy operations)
+func (s *Spoke) Walk() *[]*Job {
+	ready := []*Job{}
+
+	if len(s.jobQueue) == 0 {
+		return &ready
+	}
+
+	sort.Sort(s.jobQueue)
+	var j *Job
+	for {
+		if len(s.jobQueue) == 0 {
+			break
+		}
+
+		if time.Now().Before(s.jobQueue[0].triggerAt) {
+			break
+		}
+
+		j, s.jobQueue = s.jobQueue[0], s.jobQueue[1:]
+		delete(s.jobMap, j.id)
+		ready = append(ready, j)
+
+	}
+	return &ready
+}
+
+// CancelJob will try to delete a job that hasn't been consumed yet
+func (s *Spoke) CancelJob(id uuid.UUID) {
+	if _, ok := s.jobMap[id]; ok {
+		delete(s.jobMap, id)
+		for i, j := range s.jobQueue {
+			if j.id == id {
+				s.jobQueue = append(s.jobQueue[:i], s.jobQueue[i+1:]...)
+				break
+			}
+		}
+	}
+}
+
+// OwnsJob returns true if a job by given id is owned by this spoke
+func (s *Spoke) OwnsJob(id uuid.UUID) bool {
+	_, ok := s.jobMap[id]
+	return ok
+}
+
+// PendingJobsLen returns the number of jobs remaining in this spoke
+func (s *Spoke) PendingJobsLen() int {
+	return len(s.jobMap)
+}
+
+// ID returns the id of this spoke
+func (s *Spoke) ID() uuid.UUID {
+	return s.id
+}
+
+// -- Implement Sort.Interface on Spokes ---//
+
+// SpokesByTime implements sort.Interface for Spokes
+type SpokesByTime []*Spoke
+
+func (b SpokesByTime) Len() int {
+	return len(b)
+}
+
+func (b SpokesByTime) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
+
+func (b SpokesByTime) Less(i, j int) bool {
+	// i starts before or at the same time as j
+	return b[i].start.Sub(b[j].start) <= 0 &&
+		// && i ends before or at the same time as j
+		b[i].end.Sub(b[j].end) <= 0
+}

pkg/yaad/spoke_test.go
@@ -1,0 +1,171 @@
+package goyaad_test
+
+import (
+	"math/rand"
+	"sort"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	uuid "github.com/satori/go.uuid"
+	. "github.com/urjitbhatia/goyaad/pkg/yaad"
+)
+
+var _ = Describe("Test spokes", func() {
+	Context("Basic spoke tests", func() {
+		It("can create a spoke", func() {
+			t := time.Now()
+
+			// Starts in an hour, ends in 2 hours
+			s := NewSpoke(t.Add(time.Hour*1), t.Add(time.Hour*2))
+			Expect(s.ID()).To(BeAssignableToTypeOf(uuid.NewV4()))
+			Expect(s.IsReady()).To(BeFalse())
+			Expect(s.IsExpired()).To(BeFalse())
+
+			// Starts now, ends in 10 hours
+			s = NewSpokeFromNow(time.Hour * 10)
+			Expect(s.ID()).To(BeAssignableToTypeOf(uuid.NewV4()))
+			Expect(s.IsReady()).To(BeTrue())
+			Expect(s.IsExpired()).To(BeFalse())
+
+			// Ends in the past for the next tick
+			s = NewSpokeFromNow(0)
+			Expect(s.ID()).To(BeAssignableToTypeOf(uuid.NewV4()))
+			Expect(s.IsReady()).To(BeTrue())
+			Expect(s.IsExpired()).To(BeTrue())
+		})
+
+		It("accepts jobs into spoke", func() {
+			s := NewSpokeFromNow(time.Hour * 1)
+			j, err := NewJobAutoID(s.Start().Add(time.Second*20), nil)
+			Expect(err).To(BeNil())
+
+			// Accepts the job and returns nil
+			Expect(s.ContainsJob(j)).To(BeTrue())
+			Expect(s.AddJob(j)).To(BeNil())
+			Expect(s.PendingJobsLen()).To(Equal(1))
+			Expect(s.OwnsJob(j.ID())).To(BeTrue())
+		})
+
+		It("rejects jobs from spoke that lie outside its time bounds", func() {
+			now := time.Now()
+			s := NewSpoke(now.Add(time.Hour*1), now.Add(time.Hour*2))
+
+			// Job triggers after spoke ends
+			j, err := NewJobAutoID(s.End().Add(time.Hour*1), nil)
+			Expect(err).To(BeNil())
+
+			// Rejects the job and returns it
+			Expect(s.AddJob(j)).To(Equal(j))
+			Expect(s.PendingJobsLen()).To(Equal(0))
+
+			// Job triggers before spoke ends
+			j, err = NewJobAutoID(s.Start().Add(-10*time.Minute), nil)
+			Expect(err).To(BeNil())
+
+			// Rejects the job and returns it
+			Expect(s.AddJob(j)).To(Equal(j))
+			Expect(s.PendingJobsLen()).To(Equal(0))
+		})
+
+		It("walks empty spoke", func() {
+			s := NewSpokeFromNow(time.Minute * 1)
+			Expect(len(*s.Walk())).To(Equal(0))
+		})
+
+		It("walks spoke with jobs", func() {
+			s := NewSpokeFromNow(time.Hour * 1)
+			Expect(len(*s.Walk())).To(Equal(0))
+
+			for i := 0; i < 10; i++ {
+				j, _ := NewJobAutoID(s.Start().Add(time.Nanosecond*time.Duration(rand.Intn(900))), nil)
+				Expect(s.AddJob(j)).To(BeNil())
+			}
+			Expect(s.PendingJobsLen()).To(Equal(10))
+
+			// Wait for all jobs to be ready
+			time.Sleep(time.Second * 1)
+
+			jobs := *s.Walk()
+			Expect(len(jobs)).To(Equal(10))
+			prev := jobs[0]
+			for i := 1; i < len(jobs); i++ {
+				// Walk returns jobs in order
+				Expect(prev.TriggerAt().Sub(jobs[i].TriggerAt()) <= 0).To(BeTrue())
+			}
+		})
+
+		It("repeated walks spoke with jobs as they expire", func() {
+			s := NewSpokeFromNow(time.Hour * 1)
+			Expect(len(*s.Walk())).To(Equal(0))
+
+			// Add some jobs < 1 sec triggerAt
+			for i := 0; i < 10; i++ {
+				j, _ := NewJobAutoID(s.Start().Add(time.Nanosecond*time.Duration(rand.Intn(900))), nil)
+				Expect(s.AddJob(j)).To(BeNil())
+			}
+			Expect(s.PendingJobsLen()).To(Equal(10))
+			// Add some jobs > 1 sec triggerAt
+			for i := 0; i < 10; i++ {
+				j, _ := NewJobAutoID(s.Start().Add(time.Minute*time.Duration(10+rand.Intn(40))), nil)
+				Expect(s.AddJob(j)).To(BeNil())
+			}
+			Expect(s.PendingJobsLen()).To(Equal(20))
+
+			// Wait for all jobs to be ready
+			time.Sleep(time.Second * 1)
+
+			jobs := *s.Walk()
+			Expect(len(jobs)).To(Equal(10))
+			prev := jobs[0]
+			for i := 1; i < len(jobs); i++ {
+				// Walk returns jobs in order
+				Expect(prev.TriggerAt().Sub(jobs[i].TriggerAt()) <= 0).To(BeTrue())
+			}
+
+			// 10 jobs should remain
+			Expect(s.PendingJobsLen()).To(Equal(10))
+
+			// Walk is idempotent
+			jobs = *s.Walk()
+			Expect(len(jobs)).To(Equal(0))
+			Expect(s.PendingJobsLen()).To(Equal(10))
+		})
+
+		It("cancels job from spoke", func() {
+			s := NewSpokeFromNow(time.Hour * 1)
+			Expect(s.PendingJobsLen()).To(Equal(0))
+
+			j, _ := NewJobAutoID(s.Start().Add(time.Minute*10), nil)
+			Expect(s.AddJob(j)).To(BeNil())
+			Expect(s.PendingJobsLen()).To(Equal(1))
+
+			s.CancelJob(j.ID())
+			Expect(s.PendingJobsLen()).To(Equal(0))
+		})
+
+		It("cancels job from spoke that doesnt exist does nothing", func() {
+			s := NewSpokeFromNow(time.Hour * 1)
+			Expect(s.PendingJobsLen()).To(Equal(0))
+			s.CancelJob(uuid.NewV4())
+			s.CancelJob(uuid.NewV4())
+		})
+	})
+
+	Context("Spoke Ordering", func() {
+		It("orders spokes correctly", func() {
+			t := time.Now()
+			sone := NewSpoke(t.Add(1), t.Add(10))
+			stwo := NewSpoke(t.Add(20), t.Add(30))
+			sthree := NewSpoke(t.Add(50), t.Add(55))
+
+			spokes := SpokesByTime{stwo, sone, sthree}
+			sort.Sort(spokes)
+
+			Expect(spokes[0].ID()).To(Equal(sone.ID()))
+			Expect(spokes[1].ID()).To(Equal(stwo.ID()))
+			Expect(spokes[2].ID()).To(Equal(sthree.ID()))
+		})
+	})
+})

pkg/yaad/yaad_suite_test.go
@@ -1,0 +1,13 @@
+package goyaad_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestYaad(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Yaad Suite")
+}