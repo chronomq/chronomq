9f95adb1d088e5a10884ba3e97f993eb48476b08
Urjit Singh Bhatia <urjitsb87@gmail.com>
2019-10-01 18:04:22 +0000

    Move pkg/protocol logging to zerolog

--- pkg/protocol/yaad_suite_test.go
+++ pkg/protocol/yaad_suite_test.go
@@ -1,19 +1,14 @@
 package protocol_test
 
 import (
-	"log"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/sirupsen/logrus"
 )
 
 func TestYaad(t *testing.T) {
 	defer GinkgoRecover()
-
-	logrus.SetOutput(GinkgoWriter)
-	log.SetOutput(GinkgoWriter)
 
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "GoYaad Protocol Suite")
