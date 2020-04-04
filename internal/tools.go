7eba892d859f67b6339e2bbf59e6fdf00490497a
Urjit Singh Bhatia <urjitsb87@gmail.com>
2020-04-03 20:18:10 -0700

    go generate get protobuf/ptypes and add it to tools for vendoring

--- internal/tools.go
+++ internal/tools.go
@@ -8,4 +8,10 @@ package internal
 
 import (
 	_ "github.com/ckaznocha/protoc-gen-lint"
+	_ "github.com/golang/protobuf/protoc-gen-go"
+	_ "github.com/golang/protobuf/ptypes"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "github.com/grpc-ecosystem/grpc-gateway/runtime"
+	_ "google.golang.org/grpc"
 )
