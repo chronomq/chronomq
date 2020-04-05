c82f3abd2c643ea432d211579fa57fb92c781eb8
Urjit Singh Bhatia <urjitsb87@gmail.com>
2020-04-04 19:40:29 -0700

    bring back protoc-gen-lint tools dep to fix mod

--- internal/tools.go
+++ internal/tools.go
@@ -1,3 +1,11 @@
 // +build tools
 
 package internal
+
+// This file holds references to the various tools needed at build-time so that
+// go mod fetches them. This is the current best practice according to:
+// https://github.com/golang/go/issues/25922
+
+import (
+	_ "github.com/ckaznocha/protoc-gen-lint"
+)
